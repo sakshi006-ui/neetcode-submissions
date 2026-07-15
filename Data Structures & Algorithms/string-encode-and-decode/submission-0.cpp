d289ca1e1e7f22100c7d417d6feea4601e736f36
Sakshi Bobade <bobadesakshi9@gmail.com>
2026-07-15 10:56:36 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string s;
+    string encode(vector<string>& strs) {
+        for(string str : strs){
+            int n = str.length();
+            s += to_string(n);
+            s += "#";
+            s += str;
+        }
+        return s;
+    }
+
+    vector<string> decode(string s) {
+        int i=0;
+        int j=0;
+        vector<string>res;
+        while(i<s.length()){
+            j=i;
+            while(s[j] != '#'){
+                j++;
+            }
+            int len =0;
+            for (int k = i; k < j; k++) {
+                len = len * 10 + (s[k] - '0');
+            }
+            j++;
+            i=j;
+            string ans = s.substr(i, len);
+            res.push_back(ans);
+            i=j+len;
+        }
+        return res;
+    }
+};
